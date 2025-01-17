87d338b2c01c99942c10fd6096b5f9a12a54a399
Jedyokey <jedyokey29@gmail.com>
2025-01-17 23:43:04 +0000

    Hero and Item component

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Item.css"
+
+const Item = ({ name, image, newPrice, oldPrice }) => {
+  return (
+    <div className='item'>
+        <img src={image} alt="" />
+        <p>{name}</p>
+
+        <div className="item-prices">
+            <div className="item-price-new">
+                ${newPrice}
+            </div>
+            <div className="item-price-old">
+                ${oldPrice}
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Item
