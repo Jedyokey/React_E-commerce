dbaf350035478a4bdf68158491897b586da9cbbc
Jedyokey <jedyokey29@gmail.com>
2025-01-18 13:35:33 +0000

    Navbar and Footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Shop from './pages/Shop';
+import ShopCategory from './pages/ShopCategory';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import LoginSignUp from './pages/LoginSignUp';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/men" element={<ShopCategory category="men" />} />
+          <Route path="/women" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignUp />} />
+        </Routes>
+        <Footer />
+      </Router>
       
     </div>
   );
