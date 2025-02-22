8ac6526893781996f1e4a107bd74e186bc441179
Jedyokey <jedyokey29@gmail.com>
2025-02-22 15:07:21 +0000

    added responsiveness to newsletter, new collection and popular components

--- src/components/NewCollections/NewCollections.jsx
+++ src/components/NewCollections/NewCollections.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import "./NewCollections.css"
-import new_collections from '../assets/new_collections'
-import Item from '../Item/Item'
+import React, { forwardRef } from 'react';
+import "./NewCollections.css";
+import new_collections from '../assets/new_collections';
+import Item from '../Item/Item';
 
-const NewCollections = () => {
+const NewCollections = forwardRef((_, ref) => {
   return (
-    <div className='new-collections'>
-        <h1>NEW COLLECTIONS</h1>
-        <hr />
-        <div className="collections">
-            {new_collections.map(item => {
-                return (
-                    <Item 
-                        key={item.id} 
-                        id={item.id} 
-                        name={item.name}
-                        image={item.image}
-                        newPrice={item.new_price}
-                        oldPrice={item.old_price} 
-                    />
-                )
-            })}
-        </div>
+    <div className='new-collections' ref={ref}>
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {new_collections.map(item => {
+          return (
+            <Item 
+              key={item.id} 
+              id={item.id} 
+              name={item.name}
+              image={item.image}
+              newPrice={item.new_price}
+              oldPrice={item.old_price} 
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+});
 
-export default NewCollections
+export default NewCollections;
